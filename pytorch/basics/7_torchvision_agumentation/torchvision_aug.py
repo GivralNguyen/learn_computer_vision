a62188943727cca11fa5080d5a7d53f1c492249b
quannm <cobainnguyen@gmail.com>
2021-08-21 22:24:06 +0700

    Fix csv file name

--- pytorch/basics/7_torchvision_agumentation/torchvision_aug.py
+++ pytorch/basics/7_torchvision_agumentation/torchvision_aug.py
@@ -44,28 +44,28 @@ class CatsAndDogsDataset(Dataset):
 my_transforms = transforms.Compose(
     [  # Compose makes it possible to have many transforms
         transforms.ToPILImage(),
-        transforms.Resize((256, 256)),  # Resizes (32,32) to (36,36)
-        transforms.RandomCrop((224, 224)),  # Takes a random (32,32) crop
-        transforms.ColorJitter(brightness=0.5),  # Change brightness of image
-        transforms.RandomRotation(
-            degrees=45
-        ),  # Perhaps a random rotation from -45 to 45 degrees
-        transforms.RandomHorizontalFlip(
-            p=0.5
-        ),  # Flips the image horizontally with probability 0.5
-        transforms.RandomVerticalFlip(
-            p=0.05
-        ),  # Flips image vertically with probability 0.05
-        transforms.RandomGrayscale(p=0.2),  # Converts to grayscale with probability 0.2
+        #transforms.Resize((256, 256)),  # Resizes (32,32) to (36,36)
+        #transforms.RandomCrop((224, 224)),  # Takes a random (32,32) crop
+        #transforms.ColorJitter(brightness=0.5),  # Change brightness of image
+        #transforms.RandomRotation(
+        #    degrees=45
+        #),  # Perhaps a random rotation from -45 to 45 degrees
+        #transforms.RandomHorizontalFlip(
+        #    p=0.5
+        #),  # Flips the image horizontally with probability 0.5
+        #transforms.RandomVerticalFlip(
+        #    p=0.05
+        #),  # Flips image vertically with probability 0.05
+        #transforms.RandomGrayscale(p=0.2),  # Converts to grayscale with probability 0.2
         transforms.ToTensor(),  # Finally converts PIL image to tensor so we can train w. pytorch
-        transforms.Normalize(
-            mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]
-        ),  # Note: these values aren't optimal / (value -mean)/std
+        #transforms.Normalize(
+        #    mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]
+        #),  # Note: these values aren't optimal / (value -mean)/std
     ]
 )
 
 dataset = CatsAndDogsDataset(
-    csv_file="../5_custom_dataset/cats_dogs.csv",
+    csv_file="../5_custom_dataset/train_cats_dogs.csv",
     root_dir="../5_custom_dataset/cats_dogs_resized",
     transform=my_transforms
 )
